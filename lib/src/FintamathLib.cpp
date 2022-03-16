ee683da9454e718353b9f3d46ad7a97cc256bca5
ivansobal <vaniasobol2002@gmail.com>
2022-03-17 00:03:40 +0300

    Add Kotlin source and simple GUI

--- lib/src/FintamathLib.cpp
+++ lib/src/FintamathLib.cpp
@@ -1,7 +1,15 @@
 #include "fintamath/solver/Solver.hpp"
 
-void f() {
+#include <jni.h>
+#include <string>
+#include "fintamath/solver/Solver.hpp"
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_example_fintamath_1android_MainActivity_findSolution(
+        JNIEnv* env,
+        jobject,jstring str) {
+  std::string solution_str = env->GetStringUTFChars(str, nullptr);
   Solver solver;
-  ArithmeticExpression e("1+1");
-  solver.solve(e);
+  ArithmeticExpression arithmeticExpression(solution_str);
+  std::string s=solver.solve(arithmeticExpression).toString();
+  return env->NewStringUTF(s.c_str());
 }
