8696ea06d11ba4a218bec20b84888d5b59b73026
ivansobal <vaniasobol2002@gmail.com>
2022-03-27 21:43:48 +0300

    Add realtime expression calculation

--- app/src/main/java/com/fintarin/fintamath_android/MainActivity.kt
+++ app/src/main/java/com/fintarin/fintamath_android/MainActivity.kt
@@ -1,34 +1,58 @@
 package com.fintarin.fintamath_android
 
+import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.TextView
+import android.text.Editable
+import android.text.TextWatcher
+
 import com.fintarin.fintamath_android.databinding.ActivityMainBinding
+import java.lang.Exception
+import kotlinx.coroutines.*
+import androidx.lifecycle.lifecycleScope
 
 class MainActivity : AppCompatActivity() {
-
+    private val TAG:String="Main activity calls calculator"
     private lateinit var binding: ActivityMainBinding
-    private lateinit var button: Button
+    private lateinit var currentJob: Job
+    private fun calculate(string: String):String{
+        return try {
+            findSolution(string)
+        } catch (e:Exception){
+            "Unable to calculate"
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        button=findViewById(R.id.find_solution)
-        button.setOnClickListener { view: View ->
-            binding.solution.text = findSolution(binding.toSolve.text.toString())
-        }
+        binding.toSolve.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                Dispatchers.Main.cancelChildren()
+                currentJob=lifecycleScope.launch(Dispatchers.Main) {
+                    val result:String= withContext(Dispatchers.Default){
+                            calculate(binding.toSolve.text.toString())}
+                    if(currentJob.isActive) {
+                        binding.solution.text = result
+                    }
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+        })
     }
 
     external fun findSolution(string: String): String
 
-    companion object {
-        // Used to load the 'fintamath_android' library on application startup.
+   companion object {
         init {
-            System.loadLibrary("fintamath_android")
+            System.loadLibrary("fintamath-android")
         }
     }
 }
--- lib/src/FintamathLib.cpp
+++ lib/src/FintamathLib.cpp
@@ -1,14 +1,18 @@
 #include "fintamath/solver/Solver.hpp"
 #include <jni.h>
 #include <string>
-#include "fintamath/solver/Solver.hpp"
-extern "C" JNIEXPORT jstring JNICALL
 
-Java_com_fintarin_fintamath_1android_MainActivity_findSolution(
-        JNIEnv* env,jobject,jstring str) {
+extern "C" JNIEXPORT jstring JNICALL Java_com_fintarin_fintamath_1android_MainActivity_findSolution(JNIEnv *env,
+                                                                                                    jobject,
+                                                                                                    jstring str) {
   std::string solution_str = env->GetStringUTFChars(str, nullptr);
   Solver solver;
-  ArithmeticExpression arithmeticExpression(solution_str);
-  std::string s=solver.solve(arithmeticExpression).toString();
-  return env->NewStringUTF(s.c_str());
+  try {
+      ArithmeticExpression arithmeticExpression(solution_str);
+      std::string s = solver.solve(arithmeticExpression).toString();
+      return env->NewStringUTF(s.c_str());
+  }
+  catch (std::exception& exception) {
+      return env->NewStringUTF("Unable to calculate");
+  }
 }
