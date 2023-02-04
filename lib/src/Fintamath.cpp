73bebac5c3cadb7c88cd0ca982af0fd75682e13a
fintarin <valukevichv@gmail.com>
2023-02-04 22:52:13 +0300

    Add calculations with precision=50

--- lib/src/Fintamath.cpp
+++ lib/src/Fintamath.cpp
@@ -11,7 +11,11 @@ extern "C" JNIEXPORT jstring Java_com_fintamath_calculator_Calculator_calculate(
     std::string inStr = env->GetStringUTFChars(inJStr, nullptr);
     try {
         auto outExpr = Expression(inStr);
-        return env->NewStringUTF((outExpr.solve(24) + "\n" + outExpr.toString()).c_str());
+        return env->NewStringUTF((
+                                         outExpr.toString() + "\n" +
+                                         outExpr.solve(10) + "\n" +
+                                         outExpr.solve(50)
+                                 ).c_str());
     } catch (const UndefinedException &exc) {
         return env->NewStringUTF("Undefined");
     } catch (const Exception &exc) {
