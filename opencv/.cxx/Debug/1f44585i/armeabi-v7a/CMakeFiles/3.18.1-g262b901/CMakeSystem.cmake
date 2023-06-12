b7f72aa8b90edbe0debb7dce75649db86bf86b24
CAT-i0n <rudman.iv.o@gmail.com>
2023-06-12 21:41:13 +0300

    Add opencv and tensorflow support

---
+++ opencv/.cxx/Debug/1f44585i/armeabi-v7a/CMakeFiles/3.18.1-g262b901/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Linux-6.2.10-arch1-1")
+set(CMAKE_HOST_SYSTEM_NAME "Linux")
+set(CMAKE_HOST_SYSTEM_VERSION "6.2.10-arch1-1")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
+
+include("/home/cati0n/Android/Sdk/ndk/23.1.7779620/build/cmake/android.toolchain.cmake")
+
+set(CMAKE_SYSTEM "Android-1")
+set(CMAKE_SYSTEM_NAME "Android")
+set(CMAKE_SYSTEM_VERSION "1")
+set(CMAKE_SYSTEM_PROCESSOR "armv7-a")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
